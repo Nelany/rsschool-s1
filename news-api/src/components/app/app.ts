b0c38d8739c9327a83d6e06f9b3a1c4166734922
Nelany <svetasweetka@gmail.com>
2024-02-23 14:18:05 +0100

    refactor: migrate app.js to ts

---
+++ news-api/src/components/app/app.ts
@@ -0,0 +1,32 @@
+import AppController from '../controller/controller';
+import { AppView } from '../view/appView';
+
+class App {
+    controller: AppController;
+    view: AppView;
+    constructor() {
+        this.controller = new AppController();
+        this.view = new AppView();
+    }
+
+    start() {
+        const currentSources = document.querySelector('.sources');
+
+        if (currentSources instanceof HTMLElement) {
+            currentSources.addEventListener('click', (e) =>
+                this.controller.getNews(e, (data) => {
+                    if (data && 'articles' in data) {
+                        return this.view.drawNews(data);
+                    }
+                })
+            );
+            this.controller.getSources((data) => {
+                if (data && 'sources' in data) {
+                    return this.view.drawSources(data);
+                }
+            });
+        }
+    }
+}
+
+export default App;
