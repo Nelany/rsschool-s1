c9183c992798ef4ecf3305f5178f28b4e97de56a
Nelany <svetasweetka@gmail.com>
2024-03-07 14:58:41 +0100

    feat: RSS-PZ-01 implement login form

---
+++ rss-puzzle/src/loginPage.ts
@@ -0,0 +1,39 @@
+export function createLoginPage() {
+  const loginPageHTML = `
+      <div class="content">
+          <h2 class="main-title">ENGLISH PUZZLE</h2>
+          <h4 class="first-description">
+              Click on words, collect phrases. <br />
+              Words can be drag and drop. Select tooltips in the menu.
+          </h4>
+          <form class="login-form" id="loginForm">
+              <div class="error-message-container">
+                  <!-- Please enter the data in the specified format: minimum
+                  <span class="letters-number">3</span> characters, letters and hyphen only. -->
+              </div>
+              <label class="first-name-label" for="firstName">First Name:</label>
+              <input
+                  class="name-input first-name-input"
+                  type="text"
+                  id="firstName"
+                  name="firstName"
+
+                  title="Minimum 3 characters, letters and hyphen only"
+                  required
+              />
+              <label class="last-name-label" for="lastName">Last Name:</label>
+              <input
+                  class="name-input last-name-input"
+                  type="text"
+                  id="lastName"
+                  name="lastName"
+
+                  title="Minimum 4 characters, letters and hyphen only"
+                  required
+              />
+              <button class="button button-login" id="loginForm" type="submit">Login</button>
+          </form>
+      </div>
+  `;
+  document.body.innerHTML = loginPageHTML;
+}
--- rss-puzzle/webpack.config.js
+++ rss-puzzle/webpack.config.js
@@ -6,45 +6,45 @@ const EslintPlugin = require('eslint-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const baseConfig = {
-    entry: path.resolve(__dirname, './src/index'),
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.ts$/i,
-                use: 'ts-loader',
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.ts', '.js'],
-    },
-    output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, './dist'),
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, './src/index.html'),
-            filename: 'index.html',
-        }),
-        new CleanWebpackPlugin(),
-        new EslintPlugin({
-            extensions: 'ts',
-        }),
-        new CopyWebpackPlugin({
-            patterns: [{ from: 'src/assets/images' }],
-        }),
+  entry: path.resolve(__dirname, './src/index'),
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.ts$/i,
+        use: 'ts-loader',
+      },
     ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, './dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+      filename: 'index.html',
+    }),
+    new CleanWebpackPlugin(),
+    new EslintPlugin({
+      extensions: 'ts',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: 'src/assets/images' }],
+    }),
+  ],
 };
 
 module.exports = ({ mode }) => {
-    const isProductionMode = mode === 'prod';
-    const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+  const isProductionMode = mode === 'prod';
+  const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
 
-    return merge(baseConfig, envConfig);
+  return merge(baseConfig, envConfig);
 };
