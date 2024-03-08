d1fd1f5025687320cae1e867456fbcf87e8de5a2
Nelany <87783528+Nelany@users.noreply.github.com>
2024-03-08 21:27:34 +0100

    feat: RSS-PZ-08 Word Cards Randomization and Interaction (#95)

--- rss-puzzle/src/mainPage.ts
+++ rss-puzzle/src/mainPage.ts
@@ -1,14 +1,20 @@
+import { sentenceHandler } from './sentences';
+
+const currentSentence = 'The students agree they have too much homework';
+
 export function createMainPage() {
   const contentElement = document.querySelector('.content');
   if (contentElement) {
     contentElement.classList.add('content-fadeout');
     const mainHTML = `
   <div class="content content-fadeout main-content">
-      <h2 class="main-title">MAIN PAGE</h2>
+  <div class="field"></div>
+  <div class="line"></div>
   </div>
 `;
     setTimeout(() => {
       document.body.innerHTML = mainHTML;
+      sentenceHandler(currentSentence);
     }, 1000);
   }
 }
