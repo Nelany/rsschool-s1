1d4a338cccfbd28eb79a2c800b23a59ca7acfe84
Nelany <svetasweetka@gmail.com>
2024-03-09 23:04:29 +0100

    feat: RSS-PZ-10 Enable Reversible Interaction with Word Cards Between Blocks

---
+++ rss-puzzle/src/addLineListener.ts
@@ -0,0 +1,19 @@
+import { addFieldListener } from './addFieldListener';
+
+export function addLineListener(lineWordCard: HTMLElement, fieldElement: HTMLElement, lineElement: HTMLElement) {
+  lineWordCard.addEventListener('click', () => {
+    const fieldCell = fieldElement.querySelector('.cell');
+    const selectedWord = lineWordCard.cloneNode(true);
+
+    if (fieldCell && selectedWord instanceof HTMLElement) {
+      fieldCell.innerHTML = selectedWord.innerHTML;
+      fieldCell.className = selectedWord.className;
+      fieldCell.setAttribute('style', selectedWord.getAttribute('style') || '');
+      lineWordCard.remove();
+
+      if (fieldCell instanceof HTMLElement) {
+        addFieldListener(fieldCell, lineElement, fieldElement);
+      }
+    }
+  });
+}
