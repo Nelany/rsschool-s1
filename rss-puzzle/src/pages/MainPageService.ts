ada38141ddf8337fcbe7cc6c5b5cc3e6f115cc65
Nelany <svetasweetka@gmail.com>
2024-03-13 11:41:41 +0100

    feat: RSS-PZ-13 transform Check Button To Continue

--- rss-puzzle/src/pages/MainPageService.ts
+++ rss-puzzle/src/pages/MainPageService.ts
@@ -24,13 +24,27 @@ export function continueButtonHandler() {
   const field = document.querySelector('.field');
   const continueButton = document.querySelector('.button-continue');
   const line = document.querySelector('.line');
+  const checkButton = document.querySelector('.button-check');
+
   if (field instanceof HTMLElement && continueButton instanceof HTMLElement && line instanceof HTMLElement) {
     line.remove();
     if (wordCollections.isResetField) {
       field.innerHTML = '';
     }
     SentenceLine.draw(field);
-    continueButton.classList.add('disabled');
+    if (!checkButton) {
+      return;
+    }
+    continueButton.classList.add('disappearing');
+    setTimeout(() => {
+      continueButton.classList.add('hidden');
+      continueButton.classList.remove('disappearing');
+      checkButton.classList.remove('hidden');
+      checkButton.classList.add('semi-appearing');
+    }, 500);
+    setTimeout(() => {
+      checkButton.classList.remove('semi-appearing');
+    }, 2000);
   }
 }
 
@@ -43,10 +57,17 @@ export function checkButtonHandler() {
   if (wordCollections.getCurrentSentence() === answerArray.join(' ')) {
     const continueButton = document.querySelector('.button-continue');
     if (continueButton) {
-      continueButton.classList.remove('disabled');
+      checkButton.classList.add('disabled');
+      setTimeout(() => {
+        checkButton.classList.add('hidden');
+        continueButton.classList.remove('hidden');
+        continueButton.classList.remove('disabled');
+        continueButton.classList.add('button-green');
+      }, 100);
+      setTimeout(() => {
+        continueButton.classList.remove('button-green');
+      }, markTimeout);
     }
-
-    checkButton.classList.add('disabled');
 
     return;
   }
