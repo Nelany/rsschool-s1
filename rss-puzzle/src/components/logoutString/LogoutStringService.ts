533c9a64799d46b1a0bc47aa311ee382544ca6c8
Nelany <87783528+Nelany@users.noreply.github.com>
2024-03-18 16:04:07 +0100

    feat: RSS-PZ-24 Hint State Persistence and Default Settings (#114)

--- rss-puzzle/src/components/logoutString/LogoutStringService.ts
+++ rss-puzzle/src/components/logoutString/LogoutStringService.ts
@@ -5,6 +5,7 @@ export function removeUserDataAndOpenLoginPage(): void {
   localStorage.removeItem('lustCompletedRoundIndex');
   localStorage.removeItem('lustCompletedLevelIndex');
   localStorage.removeItem('isOffTranslate');
+  localStorage.removeItem('isOffSpeaker');
   localStorage.removeItem('isOffSpeak');
   localStorage.removeItem('levelArray');
   localStorage.removeItem('roundArray0');
